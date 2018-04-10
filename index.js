4c3a4d6da4e7ff448695dce700066c13d3736c35
陈欢斌 <1084271324@qq.com>
2018-04-10 18:12:42 +0800

    0.1.5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let fs = require('fs');
-let path = require('path');
+let fs = require("fs");
+let path = require("path");
 
-let _ = require('lodash');
-let loaderUtils = require('loader-utils');
+let _ = require("lodash");
+let loaderUtils = require("loader-utils");
 
 // 缓存
 let viewCache = {};
@@ -11,22 +11,22 @@ let viewCache = {};
 function preComplie(source, rootPath, relativePath) {
     let replaced = function(source, rootPath, relativePath) {
         // 去掉html的注释内容, 因为里面可能有模板字段逻辑, 那么下面template方法就会报错
-        source = source.replace(/<!--[\w\W]*-->/gm, ''); 
+        source = source.replace(/<!--[\w\W]*-->/gm, "");
 
         return source.replace(/<%\s*(include.+)\s*%>/g, function(match, code) {
             let part = code.split(/\s/)[1];
             let char = part.charAt(0);
             let basicPath = relativePath;
 
-            if (char === '/') {
+            if (char === "/") {
                 // 根路径
                 basicPath = rootPath;
-            } else if (char === '.' || char === '..') {
+            } else if (char === "." || char === "..") {
                 // 相对路径
                 basicPath = relativePath;
             } else {
                 // 直接就是文件名
-                part = '../' + part;
+                part = "../" + part;
             }
 
             let filePath = path.join(basicPath, part);
@@ -37,7 +37,7 @@ function preComplie(source, rootPath, relativePath) {
                         `\x1B[31m 模板路径: ${filePath}不存在 \x1B[39m`
                     );
                 } else {
-                    viewCache[filePath] = fs.readFileSync(filePath, 'utf-8');
+                    viewCache[filePath] = fs.readFileSync(filePath, "utf-8");
                 }
             }
 
@@ -73,14 +73,14 @@ module.exports = function(source) {
     this.cacheable && this.cacheable();
 
     // 普通的html
-    if (source.indexOf('<%') === -1 || source.indexOf('%>') === -1) {
+    if (source.indexOf("<%") === -1 || source.indexOf("%>") === -1) {
         return source;
     }
 
     // Handle filenames (#106)
     let webpackRemainingChain = loaderUtils
         .getRemainingRequest(this)
-        .split('!');
+        .split("!");
 
     let filename = webpackRemainingChain[webpackRemainingChain.length - 1];
 
@@ -90,25 +90,27 @@ module.exports = function(source) {
     let config = _.extend({}, query, options);
 
     // 支持include语法
-    let rootPath = config.rootPath ? path.join(process.cwd(), config.rootPath) : process.cwd();
+    let rootPath = config.rootPath
+        ? path.join(process.cwd(), config.rootPath)
+        : process.cwd();
     source = preComplie(source, rootPath, filename);
 
-    ['escape', 'interpolate', 'evaluate'].forEach(function(templateSetting) {
+    ["escape", "interpolate", "evaluate"].forEach(function(templateSetting) {
         let setting = config[templateSetting];
         if (_.isString(setting)) {
-            config[templateSetting] = new RegExp(setting, 'g');
+            config[templateSetting] = new RegExp(setting, "g");
         }
     });
 
     let template = _.template(source, config);
     let exportsString = "module.exports = ";
 
-	if (config.export === 'es6') {
+    if (config.export === "es6") {
         exportsString = "export default ";
-	} else if (config.export === 'return') {
-        exportsString = 'return '
-	} else if (config.export === '') {
-        exportsString = '';
+    } else if (config.export === "return") {
+        exportsString = "return ";
+    } else if (config.export === "") {
+        exportsString = "";
     }
 
     return exportsString + template;
